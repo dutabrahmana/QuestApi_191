a2978ac5d155863fc543afbb7f7d3761dcee37ab
dutabrahmana <dutalinggau25@gmail.com>
2025-12-19 10:40:46 +0700

    Implementasi objek DestinasiHome untuk mendefinisikan rute dan judul halaman utama pada sistem navigasi aplikasi.

--- app/src/main/java/com/example/api/uicontroller/route/DestinasiHome.kt
+++ app/src/main/java/com/example/api/uicontroller/route/DestinasiHome.kt
@@ -1,4 +1,8 @@
 package com.example.api.uicontroller.route
 
-object DestinasiHome {
+import com.example.api.R
+
+object DestinasiHome: DestinasiNavigasi{
+    override val route = "home"
+    override val titleRes = R.string.nama
 }
