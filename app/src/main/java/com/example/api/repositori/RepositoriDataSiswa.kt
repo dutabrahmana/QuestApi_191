7effc26f33fb00e729205a93fbfdab89293fc713
dutabrahmana <dutalinggau25@gmail.com>
2025-12-19 10:27:37 +0700

    Mengimplementasikan layer repositori untuk data siswa dengan menambahkan interface RepositoryDataSiswa dan class JaringanRepositoryDataSiswa. Implementasi ini berfungsi sebagai abstraksi untuk mengambil data dari API dan mengirimkan data baru menggunakan ServiceApiSiswa.

--- app/src/main/java/com/example/api/repositori/RepositoriDataSiswa.kt
+++ app/src/main/java/com/example/api/repositori/RepositoriDataSiswa.kt
@@ -1,2 +1,17 @@
 package com.example.api.repositori
 
+import com.example.api.apiservice.ServiceApiSiswa
+import com.example.api.modeldata.DataSiswa
+
+interface RepositoryDataSiswa {
+    suspend fun getDataSiswa(): List<DataSiswa>
+    suspend fun postDataSiswa(dataSiswa: DataSiswa):retrofit2.Response<Void>
+}
+
+class jaringanRepositoryDataSiswa (
+    private val serviceApiSiswa: ServiceApiSiswa
+): RepositoryDataSiswa{
+    override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
+    override suspend fun postDataSiswa(dataSiswa: DataSiswa):retrofit2
+    .Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+}
