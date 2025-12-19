8a070894da56f33391143b220d62b9325c36cd68
dutabrahmana <dutalinggau25@gmail.com>
2025-12-19 18:01:58 +0700

    Mengimplementasikan sistem navigasi utama aplikasi dengan menambahkan DataSiswaApp dan HostNavigasi. Implementasi ini menggunakan NavHost untuk mengatur alur perpindahan layar dari halaman utama (HomeScreen) ke halaman input (EntrySiswaScreen) serta menyediakan kerangka navigasi menuju halaman detail berdasarkan ID siswa.

--- app/src/main/java/com/example/api/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/api/uicontroller/PetaNavigasi.kt
@@ -1,2 +1,49 @@
 package com.example.api.uicontroller
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.api.uicontroller.route.DestinasiEntry
+import com.example.api.uicontroller.route.DestinasiHome
+import com.example.api.uicontroller.route.DestinasiDetail
+import com.example.api.view.EntrySiswaScreen
+import com.example.api.view.HomeScreen
+
+@Composable
+fun DataSiswaApp(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier
+) {
+    HostNavigasi(navController = navController)
+}
+
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier
+    ) {
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
+                navigateToItemUpdate = {
+                    navController.navigate("${DestinasiDetail.route}/${it}")
+                }
+            )
+        }
+        composable(DestinasiEntry.route) {
+            EntrySiswaScreen(
+                navigateBack = {
+                    navController.navigate(DestinasiHome.route)
+                }
+            )
+        }
+    }
+}
