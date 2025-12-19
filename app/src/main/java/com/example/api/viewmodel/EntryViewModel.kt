4a006b2f52c2462d28da1b20386775c19a4043fe
dutabrahmana <dutalinggau25@gmail.com>
2025-12-19 17:59:48 +0700

    Mengimplementasikan EntryViewModel untuk menangani proses penambahan data siswa. Implementasi ini mencakup manajemen state melalui uiStateSiswa, validasi input formulir agar tidak kosong, serta fungsi addSiswa untuk mengirimkan data ke API melalui repositori dengan penanganan respon sukses maupun error.

--- app/src/main/java/com/example/api/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/api/viewmodel/EntryViewModel.kt
@@ -1,3 +1,58 @@
 package com.example.api.viewmodel
 
-data class EntryViewModel()
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.api.modeldata.DetailSiswa
+import com.example.api.modeldata.UIStateSiswa
+import com.example.api.modeldata.toDataSiswa
+import com.example.api.repositori.RepositoryDataSiswa
+
+class EntryViewModel(private val repositoryDataSiswa: RepositoryDataSiswa) :
+    ViewModel() {
+
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    /* Fungsi untuk memvalidasi input */
+    private fun validasiInput(
+        uiState: DetailSiswa = uiStateSiswa.detailSiswa
+    ): Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+
+    //Fungsi untuk menangani saat ada perubahan pada text input
+    fun updateUiState(detailSiswa: DetailSiswa) {
+        uiStateSiswa =
+            UIStateSiswa(
+                detailSiswa = detailSiswa,
+                isEntryValid = validasiInput(detailSiswa)
+            )
+    }
+
+    /* Fungsi untuk menyimpan data yang di-entry */
+    suspend fun addSiswa() {
+        if (!validasiInput()) return
+
+        try {
+            val response = repositoryDataSiswa.postDataSiswa(
+                uiStateSiswa.detailSiswa.toDataSiswa()
+            )
+
+            if (response.isSuccessful) {
+                println("✅ Sukses Tambah Data")
+            } else {
+                println("❌ Gagal tambah data: ${response.code()}")
+            }
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+            println("🔥 ERROR addSiswa: ${e.message}")
+        }
+    }
+
+
+}
