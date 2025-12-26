d175b8d40c5e427e729a3ab3c08b936226e1086a
dutabrahmana <dutalinggau25@gmail.com>
2025-12-26 20:25:35 +0700

    Mengimplementasikan interface ServiceApiSiswa sebagai kontrak layanan API menggunakan Retrofit. Perubahan ini mencakup pendefinisian endpoint untuk operasi CRUD, seperti pengambilan seluruh daftar siswa, penambahan data baru, pembaruan data, hingga penghapusan siswa berdasarkan ID, dengan menggunakan metode HTTP yang sesuai (GET, POST, PUT, DELETE) serta dukungan untuk response handling yang asinkron.

--- app/src/main/java/com/example/api/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/api/apiservice/ServiceApiSiswa.kt
@@ -8,7 +8,6 @@ import retrofit2.http.POST
 import retrofit2.http.PUT
 import retrofit2.http.Query
 
-
 interface ServiceApiSiswa {
     @GET("bacaTeman.php")
     suspend fun getSiswa(): List<DataSiswa>
@@ -16,12 +15,12 @@ interface ServiceApiSiswa {
     @POST("insertTM.php")
     suspend fun postSiswa(@Body dataSiswa: DataSiswa):retrofit2.Response<Void>
 
-    @GET ("baca1Teman.php/{id}")
-    suspend fun getSatuSiswa(@Query("id") id : Int) : DataSiswa
+    @GET("baca1teman.php/{id}")
+    suspend fun getSatuSiswa (@Query("id") id: Int): DataSiswa
 
     @PUT("editTM.php/{id}")
-    suspend fun editSatuSiswa(@Query("id") id:Int, @Body dataSiswa: DataSiswa): retrofit2.Response<Void>
+    suspend fun editSatuSiswa (@Query("id") id:Int,@Body datasiswa: DataSiswa):retrofit2.Response<Void>
 
-    @DELETE("deleteTM.php/{id}")
-    suspend fun hapusSatuSiswa(@Query("id") id:Int):retrofit2.Response<Void>
+    @DELETE("hapusTM.php/{id}")
+    suspend fun hapusSatuSiswa (@Query("id") id: Int):retrofit2.Response<Void>
 }
