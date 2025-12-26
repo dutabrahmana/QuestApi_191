7e27554939451fdb76d02ae1a34afe4b804b1db1
dutabrahmana <dutalinggau25@gmail.com>
2025-12-26 20:32:16 +0700

    Mengimplementasikan layer repositori dengan membuat interface RepositoryDataSiswa dan implementasinya jaringanRepositoryDataSiswa. Perubahan ini berfungsi sebagai abstraksi untuk memisahkan logika pengambilan data dari sumber eksternal (API) dengan bagian aplikasi lainnya. Repositori ini menyediakan metode lengkap untuk operasi CRUD siswa, yang menghubungkan langsung pemanggilan fungsi ke ServiceApiSiswa menggunakan Retrofit.

--- app/src/main/java/com/example/api/repositori/RepositoriDataSiswa.kt
+++ app/src/main/java/com/example/api/repositori/RepositoriDataSiswa.kt
@@ -1,11 +1,17 @@
 package com.example.api.repositori
 
-import com.example.api.apiservice.ServiceApiSiswa
+
+
 import com.example.api.modeldata.DataSiswa
+import com.example.api.apiservice.ServiceApiSiswa
 
 interface RepositoryDataSiswa {
     suspend fun getDataSiswa(): List<DataSiswa>
     suspend fun postDataSiswa(dataSiswa: DataSiswa):retrofit2.Response<Void>
+    suspend fun getSatuSiswa(id:Int) : DataSiswa
+    suspend fun editSatuSiswa (id:Int,datasiswa: DataSiswa) : retrofit2.Response<Void>
+    suspend fun hapusSatuSiswa (id:Int) : retrofit2.Response<Void>
+
 }
 
 class jaringanRepositoryDataSiswa (
@@ -14,4 +20,10 @@ class jaringanRepositoryDataSiswa (
     override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
     override suspend fun postDataSiswa(dataSiswa: DataSiswa):retrofit2
     .Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+    override suspend fun getSatuSiswa(id: Int): DataSiswa = serviceApiSiswa.getSatuSiswa(id)
+    override suspend fun editSatuSiswa(id: Int, dataSiswa: DataSiswa): retrofit2.Response<Void> =
+        serviceApiSiswa.editSatuSiswa(id, dataSiswa)
+    override suspend fun hapusSatuSiswa(id: Int): retrofit2.Response<Void> =
+        serviceApiSiswa.hapusSatuSiswa(id)
+
 }
