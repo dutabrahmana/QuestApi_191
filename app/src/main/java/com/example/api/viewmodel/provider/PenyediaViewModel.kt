e7b1d23200ef1e41957f1b6fcaf39db613dcf8cb
dutabrahmana <dutalinggau25@gmail.com>
2025-12-26 20:31:09 +0700

    Mengimplementasikan PenyediaViewModel menggunakan viewModelFactory untuk memfasilitasi injeksi dependensi pada seluruh ViewModel di aplikasi. Perubahan ini mencakup penyediaan instance HomeViewModel, EntryViewModel, DetailViewModel, dan EditViewModel dengan menghubungkannya langsung ke RepositoryDataSiswa melalui container aplikasi. Selain itu, digunakan createSavedStateHandle() untuk mendukung pengambilan argumen navigasi secara otomatis pada ViewModel yang membutuhkan data spesifik seperti ID siswa.

--- app/src/main/java/com/example/api/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/api/viewmodel/provider/PenyediaViewModel.kt
@@ -1,11 +1,14 @@
 package com.example.api.viewmodel.provider
 
+
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.api.repositori.AplikasiDataSiswa
+import com.example.api.viewmodel.DetailViewModel
+import com.example.api.viewmodel.EditViewModel
 import com.example.api.viewmodel.EntryViewModel
 import com.example.api.viewmodel.HomeViewModel
 
@@ -26,6 +29,18 @@ object PenyediaViewModel {
             EntryViewModel(
                 aplikasiDataSiswa().container
                     .repositoryDataSiswa
+            )
+        }
+        initializer {
+            DetailViewModel(
+                this.createSavedStateHandle(),
+                aplikasiDataSiswa().container.repositoryDataSiswa
+            )
+        }
+        initializer {
+            EditViewModel(
+                this.createSavedStateHandle(),
+                aplikasiDataSiswa().container.repositoryDataSiswa
             )
         }
     }
