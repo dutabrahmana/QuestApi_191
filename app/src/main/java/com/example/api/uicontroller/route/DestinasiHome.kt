0775a51845c63b4c9711889427e8acd8951d13af
dutabrahmana <dutalinggau25@gmail.com>
2025-12-26 20:23:44 +0700

    Menambahkan objek DestinasiHome sebagai rute navigasi utama aplikasi. Implementasi ini menetapkan rute "home" dan mengaitkan resource judul halaman dari R.string.nama, guna memastikan konsistensi navigasi pada halaman daftar siswa atau beranda.

--- app/src/main/java/com/example/api/uicontroller/route/DestinasiHome.kt
+++ app/src/main/java/com/example/api/uicontroller/route/DestinasiHome.kt
@@ -2,6 +2,7 @@ package com.example.api.uicontroller.route
 
 import com.example.api.R
 
+
 object DestinasiHome: DestinasiNavigasi{
     override val route = "home"
     override val titleRes = R.string.nama
